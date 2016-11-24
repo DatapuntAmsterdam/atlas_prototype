6b251cb9cfbd390036bda057ecf7faaaddfb5f4d
Erik Nijland <e.n.nijland@gmail.com>
2016-11-24 11:24:40 +0100

    Now using Avenir Heavy

--- modules/shared/services/environment/environment-config.constant.js
+++ modules/shared/services/environment/environment-config.constant.js
@@ -5,7 +5,7 @@
         .module('dpShared')
         .constant('ENVIRONMENT_CONFIG', {
             DEVELOPMENT: {
-                API_ROOT: 'https://api-acc.datapunt.amsterdam.nl/',
+                API_ROOT: 'https://api.datapunt.amsterdam.nl/',
                 AUTH_ROOT: 'https://api-acc.datapunt.amsterdam.nl/authenticatie/'
             },
             PRODUCTION: {
