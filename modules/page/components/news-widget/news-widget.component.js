3590817fa0eddc73eb6511c646dc73d1a4c44bf2
vegter <amsjurien@gmail.com>
2017-02-13 08:34:42 +0100

    Improve news entries parsing

--- modules/page/components/news-widget/news-widget.component.js
+++ modules/page/components/news-widget/news-widget.component.js
@@ -26,20 +26,17 @@
 
             // Extract entries
             vm.newsEntries = news.feed.entry.map(entry => {
-                let result = {
-                    id: entry.title.$t
-                };
-
                 // Extract the contents
-                let content = entry.content.$t;
-                content.split(/(^|, )attr/).forEach(keyValue => {
-                    if (keyValue) {
-                        let i = keyValue.indexOf(':');
-                        let key = keyValue.substring(0, i);
-                        let value = keyValue.substring(i + 1);
-                        result[key] = value;
-                    }
-                });
+                let result = entry.content.$t   // attrx: value, attry: value, ....
+                    .replace(/^attr/, '')       // x: value, attry: value, ...
+                    .split(/, attr/)            // [x:value, y:value, ...]
+                    .map(keyValue => keyValue.split(/: ([^]*)/))    // [[x, value], [y, value], ...
+                    .reduce((item, [key, value]) => {
+                        item[key] = value;      // item.x = value, item.y = value, ...
+                        return item;
+                    }, {
+                        id: entry.title.$t      // start with setting the item.id
+                    });
 
                 result.contents = $sce.trustAsHtml(markdownParser.parse(result.contents));
 
