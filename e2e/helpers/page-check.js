f26701991e7abd76a3f12799ea74b4bc5d4c558f
vegter <amsjurien@gmail.com>
2017-02-14 08:39:13 +0100

    Test for RegExp

--- e2e/helpers/page-check.js
+++ e2e/helpers/page-check.js
@@ -2,7 +2,7 @@
 
 module.exports = function (actual, expected) {
     Object.keys(expected).forEach(key => {
-        if (typeof expected[key] === 'object') {    // Regular expression
+        if (expected[key] instanceof RegExp) {    // Regular expression
             expect(actual[key]).toMatch(expected[key]);
         } else {                                    // string, number, boolean
             expect(actual[key]).toBe(expected[key]);
