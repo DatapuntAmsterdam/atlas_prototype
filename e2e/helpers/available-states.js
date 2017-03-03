e0d189e3931e977e9690168827197fa39d7632a4
eriknijland <Fender12!>
2017-03-03 16:27:50 +0100

    Updated the kastraal object test case with an object that has all 5 of these thingies: bepekringen, aantekeningne, ontstaan uit, betrokken bij en zakelijke rechten.

--- e2e/helpers/available-states.js
+++ e2e/helpers/available-states.js
@@ -26,8 +26,8 @@ module.exports = {
         validator: require('../validators/map_data-selection')
     },
     'MAP_DETAIL--KADASTRAAL-OBJECT': {
-        url: '#?dte=https:%2F%2Fapi.datapunt.amsterdam.nl%2Fbrk%2Fobject%2FNL.KAD.OnroerendeZaak.11820015470000%2F&mp' +
-            'b=topografie&mpz=13&mpv=52.3530715:5.0008373',
+        url: '#?dte=https:%2F%2Fapi-acc.datapunt.amsterdam.nl%2Fbrk%2Fobject%2FNL.KAD.OnroerendeZaak.14110053810001%2' +
+            'F&mpb=topografie&mpz=16&mpv=52.3785997:4.8457623',
         validator: require('../validators/map_detail--kadastraal-object')
     },
     'MAP_DETAIL--KADASTRAAL-SUBJECT-NATUURLIJK-PERSOON': {
