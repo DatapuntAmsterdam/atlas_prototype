bd119a38f36d2c9c42f660a905eda30331bdb570
vegter <amsjurien@gmail.com>
2017-03-30 15:07:22 +0200

    Wait for access token

--- modules/shared/services/api/api.factory.js
+++ modules/shared/services/api/api.factory.js
@@ -5,13 +5,65 @@
         .module('dpShared')
         .factory('api', apiFactory);
 
-    apiFactory.$inject = ['$http', 'user', 'sharedConfig'];
+    apiFactory.$inject = ['$interval', '$q', '$http', 'user', 'sharedConfig'];
 
-    function apiFactory ($http, user, sharedConfig) {
+    function apiFactory ($interval, $q, $http, user, sharedConfig) {
         return {
             getByUrl,
             getByUri
         };
+
+        // function getByUrl2 (url, params, cancel) {
+        //     const token = user.getAccessToken();
+        //     if (token) {
+        //         return getByUrlWithToken(url, params, cancel, token);
+        //     } else if (user.getRefreshToken()) {
+        //         // user is logging in, refresh token is available, access token not yet
+        //         const defer = $q.defer();
+        //
+        //         const interval = $interval(() => {
+        //             if (!user.getRefreshToken() || user.getAccessToken()) {
+        //                 // Refresh token was invalid or access token has been received
+        //                 $interval.cancel(interval);
+        //                 defer.resolve(getByUrl(url, params, cancel));
+        //             }
+        //         }, 250, 20);    // try every 1/4 second, for max 20 * 250 = 5 seconds
+        //
+        //         // On interval ends resolve without a token
+        //         // interval.then(() => {
+        //         //     defer.resolve(getByUrlWithToken(url, params, cancel, null));
+        //         // });
+        //
+        //         return defer.promise;
+        //     }
+        // }
+        //
+
+        function getByUrl (url, params, cancel) {
+            const token = user.getAccessToken();
+            if (token) {
+                return getByUrlWithToken(url, params, cancel, user.getAccessToken());
+            }
+            // else if (user.getRefreshToken()) {
+            //     // user is logging in, refresh token is available, access token not yet
+            //     const defer = $q.defer();
+            //
+            //     const interval = $interval(() => {
+            //         if (!user.getRefreshToken() || user.getAccessToken()) {
+            //             // Refresh token was invalid or access token has been received
+            //             $interval.cancel(interval);
+            //             defer.resolve(getByUrl(url, params, cancel));
+            //         }
+            //     }, 250, 20);    // try every 1/4 second, for max 20 * 250 = 5 seconds
+            //
+            //     // On interval ends resolve without a token
+            //     // interval.then(() => {
+            //     //     defer.resolve(getByUrlWithToken(url, params, cancel, null));
+            //     // });
+            //
+            //     return defer.promise;
+            // }
+        }
 
         /**
          *
@@ -20,10 +72,9 @@
          * @param {Promise} cancel - an optional promise ($q.defer()) to be able to cancel the request
          * @returns {Promise}
          */
-        function getByUrl (url, params, cancel) {
+        function getByUrlWithToken (url, params, cancel, token) {
             const headers = {};
 
-            const token = user.getAccessToken();
             if (token) {
                 headers.Authorization = sharedConfig.AUTH_HEADER_PREFIX + token;
             }
