d441b7354bacf37ddfa96601ab6f720c28f00f95
royderks <hello@hackteam.io>
2019-01-11 15:34:09 +0100

    Add piwik events for dataselection

--- src/store/middleware/piwikMiddleware.js
+++ src/store/middleware/piwikMiddleware.js
@@ -1,7 +1,9 @@
 import piwikTracker from '../../shared/services/piwik-tracker/piwik-tracker';
 import { routing } from '../../app/routes';
 import { DOWNLOAD_DATASET_RESOURCE } from '../../shared/ducks/datasets/data/data';
+import { DOWNLOAD_DATA_SELECTION } from '../../shared/ducks/data-selection/constants';
 import { AUTHENTICATE_USER_REQUEST, AUTHENTICATE_USER_SUCCESS } from '../../shared/ducks/user/user';
+import { ADD_FILTER, REMOVE_FILTER } from '../../shared/ducks/filters/filters';
 
 /** Set Piwik variable */
 const actionsToPiwik = {
@@ -22,6 +24,12 @@ const actionsToPiwik = {
     'Download',
     tracking.dataset,
     tracking.resourceUrl
+  ],
+  [DOWNLOAD_DATA_SELECTION]: (tracking) => [
+    'trackEvent',
+    'Download-table',
+    `dataselectie-download-${tracking.toLowerCase()}`,
+    null
   ],
   SET_MAP_BASE_LAYER: (tracking) => [
     'trackEvent',
@@ -51,6 +59,18 @@ const actionsToPiwik = {
     'trackEvent',
     'login',
     'ingelogd',
+    tracking
+  ],
+  [ADD_FILTER]: (tracking) => [
+    'trackEvent',
+    'filter',
+    'dataselectie-tabel-filter',
+    Object.keys(tracking)[0]
+  ],
+  [REMOVE_FILTER]: (tracking) => [
+    'trackEvent',
+    'filter',
+    'dataselectie-tabel-filter-verwijder',
     tracking
   ]
 };
