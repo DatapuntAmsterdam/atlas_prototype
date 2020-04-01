ad328bc407cfd3989054d683df4ff1ec4384d033
Arjan <>
2020-04-01 11:06:32 +0200

    - Fixed e2e tests - Added hide-popup command - Added some selectors in constants - Added cypress' way handling environment variables

--- test/e2e/cypress-run.js
+++ test/e2e/cypress-run.js
@@ -31,13 +31,6 @@ return glob(`${SCRIPTS_FOLDER}/**/*`, {
         config: {
           integrationFolder: SCRIPTS_FOLDER,
         },
-        env: {
-          API_ROOT: process.env.API_ROOT,
-          USERNAME_EMPLOYEE: process.env.USERNAME_EMPLOYEE,
-          USERNAME_EMPLOYEE_PLUS: process.env.USERNAME_EMPLOYEE_PLUS,
-          PASSWORD_EMPLOYEE: process.env.PASSWORD_EMPLOYEE,
-          PASSWORD_EMPLOYEE_PLUS: process.env.PASSWORD_EMPLOYEE_PLUS,
-        },
       })
       .then((results) => {
         numFailed += results.totalFailed
