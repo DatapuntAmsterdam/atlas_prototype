f008d1843e2f131a1779f17e1c20b054e86b2792
Chris van Mook <chris@chrisvanmook.com>
2020-06-03 15:25:39 +0200

    Moved methods outside draw component

--- src/app/pages/MapPage/Components/DrawTool.tsx
+++ src/app/pages/MapPage/Components/DrawTool.tsx
@@ -22,12 +22,51 @@ type MarkerGroup = {
 }
 
 type Props = {
-  onToggle: Function
+  onToggle: (showDrawing: boolean) => void
   setMarkerGroups: Function
   markerGroupsRef: React.RefObject<MarkerGroup[]>
 }
 
 const DATA_SELECTION_ENDPOINT = 'https://api.data.amsterdam.nl/dataselectie/bag/geolocation/'
+
+const getTotalDistance = (latLngs: LatLng[]) => {
+  return latLngs.reduce(
+    (total, latlng, i) => {
+      if (i > 0) {
+        const dist = latlng.distanceTo(latLngs[i - 1])
+        return total + dist
+      }
+      return total
+    },
+    latLngs.length > 2 ? latLngs[0].distanceTo(latLngs[latLngs.length - 1]) : 0,
+  )
+}
+
+const bindDistanceAndAreaToTooltip = (layer: ExtendedLayer) => {
+  const latLngs = layer
+    .getLatLngs()
+    // @ts-ignore
+    .flat()
+    .flat()
+  const distance = getTotalDistance(latLngs)
+
+  let toolTipText: string
+
+  if (distance >= 1000) {
+    toolTipText = `${L.GeometryUtil.formattedNumber(`${distance / 1000}`, 2)} km`
+  } else {
+    toolTipText = `${L.GeometryUtil.formattedNumber(`${distance}`, 2)} m`
+  }
+
+  if (layer instanceof Polygon) {
+    toolTipText = `${L.GeometryUtil.readableArea(L.GeometryUtil.geodesicArea(latLngs), true, {
+      m: 1,
+    })}, ${toolTipText}`
+  }
+  if (toolTipText) {
+    layer.bindTooltip(toolTipText, { direction: 'bottom' }).openTooltip()
+  }
+}
 
 const DrawTool: React.FC<Props> = ({ onToggle, setMarkerGroups, markerGroupsRef }) => {
   const [mapInstance] = useState<L.Map>()
@@ -65,45 +104,6 @@ const DrawTool: React.FC<Props> = ({ onToggle, setMarkerGroups, markerGroupsRef 
 
   const editVertex = async (e: L.DrawEvents.EditVertex) => {
     await getMarkerGroup(e.poly as ExtendedLayer)
-  }
-
-  const getTotalDistance = (latLngs: LatLng[]) => {
-    return latLngs.reduce(
-      (total, latlng, i) => {
-        if (i > 0) {
-          const dist = latlng.distanceTo(latLngs[i - 1])
-          return total + dist
-        }
-        return total
-      },
-      latLngs.length > 2 ? latLngs[0].distanceTo(latLngs[latLngs.length - 1]) : 0,
-    )
-  }
-
-  const bindDistanceAndAreaToTooltip = (layer: ExtendedLayer) => {
-    const latLngs = layer
-      .getLatLngs()
-      // @ts-ignore
-      .flat()
-      .flat()
-    const distance = getTotalDistance(latLngs)
-
-    let toolTipText: string
-
-    if (distance >= 1000) {
-      toolTipText = `${L.GeometryUtil.formattedNumber(`${distance / 1000}`, 2)} km`
-    } else {
-      toolTipText = `${L.GeometryUtil.formattedNumber(`${distance}`, 2)} m`
-    }
-
-    if (layer instanceof Polygon) {
-      toolTipText = `${L.GeometryUtil.readableArea(L.GeometryUtil.geodesicArea(latLngs), true, {
-        m: 1,
-      })}, ${toolTipText}`
-    }
-    if (toolTipText) {
-      layer.bindTooltip(toolTipText, { direction: 'bottom' }).openTooltip()
-    }
   }
 
   useEffect(() => {
