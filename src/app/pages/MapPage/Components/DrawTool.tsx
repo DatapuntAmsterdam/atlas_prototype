85c1a2aad75f8b7ee3dbb34bc9974b806374547c
royderks <10717410+royderks@users.noreply.github.com>
2020-06-18 17:51:17 +0200

    Set the polygon data in the url

--- src/app/pages/MapPage/Components/DrawTool.tsx
+++ src/app/pages/MapPage/Components/DrawTool.tsx
@@ -1,23 +1,17 @@
-import L, { LatLng, LatLngTuple, Polygon, Polyline } from 'leaflet'
-import React, { useContext, useEffect } from 'react'
+import L, { LatLng, LatLngTuple, Polygon } from 'leaflet'
+import React, { useContext, useEffect, useMemo } from 'react'
 import { useMapInstance } from '@datapunt/react-maps'
-import { DrawTool as DrawToolComponent } from '@datapunt/arm-draw'
+import {
+  DrawTool as DrawToolComponent,
+  PolygonType,
+  PolylineType,
+  ExtendedLayer,
+} from '@datapunt/arm-draw'
 import { mapPanelComponents, usePanToLatLng } from '@datapunt/arm-core'
+import { themeColor, ascDefaultTheme } from '@datapunt/asc-ui'
 import { SnapPoint } from '../types'
 import DataSelectionContext from '../DataSelectionContext'
-
-type extraLayerTypes = {
-  id: string
-  editing: {
-    _enabled: boolean
-    disable: () => void
-  }
-}
-
-type PolygonType = extraLayerTypes & Polygon
-type PolylineType = extraLayerTypes & Polyline
-
-export type ExtendedLayer = PolygonType | PolylineType
+import MapContext from '../MapContext'
 
 type MarkerGroup = {
   id: string
@@ -30,6 +24,7 @@ type MarkerGroup = {
 }
 
 type Props = {
+  isOpen: boolean
   onToggle: (showDrawing: boolean) => void
   setMarkerGroups: (markerGroups: MarkerGroup[]) => void
   setDataSelectionResults: (results: any) => void
@@ -76,7 +71,7 @@ const setDistance = (layer: ExtendedLayer) => {
   return toolTipText
 }
 
-const bindDistanceAndAreaToTooltip = (layer: ExtendedLayer, toolTipText) => {
+const bindDistanceAndAreaToTooltip = (layer: ExtendedLayer, toolTipText: string) => {
   layer.bindTooltip(toolTipText, { direction: 'bottom' }).openTooltip()
 }
 
@@ -85,17 +80,27 @@ const DrawTool: React.FC<Props> = ({ onToggle, isOpen }) => {
   const { fetchData, fetchMapVisualization, mapVisualization, removeDataSelection } = useContext(
     DataSelectionContext,
   )
+  const { drawingGeometry, setDrawingGeometry } = useContext(MapContext)
+
   const mapInstance = useMapInstance()
   const { pan } = usePanToLatLng()
 
   const getData = async (layer: ExtendedLayer, distanceText: string) => {
+    const latLngs = layer.getLatLngs()
+    if (
+      drawingGeometry &&
+      drawingGeometry.map((latLng) => L.latLng(latLng)).toString() !== latLngs[0].toString()
+    ) {
+      setDrawingGeometry(latLngs[0])
+    }
+
     if (layer instanceof Polygon) {
       setPositionFromSnapPoint(SnapPoint.Halfway)
-      const latLngs = layer.getLatLngs() as LatLng[][]
-      const firstDrawingPoint = latLngs[0][0]
+
+      const firstDrawingPoint = latLngs[0]
       await fetchMapVisualization(latLngs, layer.id)
       await fetchData(
-        layer.getLatLngs() as LatLng[][],
+        latLngs,
         layer.id,
         {
           size: 20,
@@ -112,6 +117,7 @@ const DrawTool: React.FC<Props> = ({ onToggle, isOpen }) => {
 
   const editVertex = async (e: L.DrawEvents.EditVertex) => {
     const layer = e.poly as ExtendedLayer
+
     const distanceText = setDistance(layer)
     bindDistanceAndAreaToTooltip(layer, distanceText)
     await getData(layer, distanceText)
@@ -131,11 +137,42 @@ const DrawTool: React.FC<Props> = ({ onToggle, isOpen }) => {
     }
   }, [mapInstance])
 
+  const setInitialDrawing = (polybounds: Array<{ lat: number; lng: number }>) => {
+    function createPolyline(coordinates: Array<{ lat: number; lng: number }>): PolylineType {
+      // @ts-ignore
+      const bounds = coordinates.map(({ lat, lng }) => [lat, lng]) as LatLng[]
+
+      return L.polyline(bounds, {
+        color: themeColor('support', 'invalid')({ theme: ascDefaultTheme }),
+        bubblingMouseEvents: false,
+      }) as PolylineType
+    }
+
+    function createPolygon(coordinates: Array<{ lat: number; lng: number }>): PolygonType {
+      // @ts-ignore
+      const bounds = coordinates.map(({ lat, lng }) => [lat, lng]) as LatLng[]
+
+      return L.polygon(bounds, {
+        color: themeColor('support', 'invalid')({ theme: ascDefaultTheme }),
+        bubblingMouseEvents: false,
+      }) as PolygonType
+    }
+
+    return polybounds.length > 2 ? createPolygon(polybounds) : createPolyline(polybounds)
+  }
+
+  const initalDrawnItem = useMemo(() => {
+    if (drawingGeometry) return setInitialDrawing(drawingGeometry)
+
+    return ''
+  }, [drawingGeometry])
+
   return (
     <DrawToolComponent
       onDrawEnd={async (layer: ExtendedLayer) => {
         const distanceText = setDistance(layer)
         bindDistanceAndAreaToTooltip(layer, distanceText)
+
         await getData(layer, distanceText)
       }}
       onDelete={(layersInEditMode: Array<ExtendedLayer>) => {
@@ -148,6 +185,7 @@ const DrawTool: React.FC<Props> = ({ onToggle, isOpen }) => {
       }}
       isOpen={isOpen}
       onToggle={onToggle}
+      drawnItem={initalDrawnItem}
     />
   )
 }
