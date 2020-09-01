c599668577e44dbd28814048c748db5116d53f36
Jon Koops <jonkoops@gmail.com>
2020-09-01 14:29:34 +0200

    Clean up DrawMapVisualization

--- src/app/pages/MapPage/draw/DrawMapVisualization.tsx
+++ src/app/pages/MapPage/draw/DrawMapVisualization.tsx
@@ -7,37 +7,36 @@ import DataSelectionContext from './DataSelectionContext'
 
 const DrawMapVisualization: React.FC = () => {
   const { mapVisualizations } = useContext(DataSelectionContext)
+
   return (
     <>
-      {mapVisualizations.length
-        ? mapVisualizations.map((mapVisualization) => {
-            switch (mapVisualization.type) {
-              case DataSelectionMapVisualizationType.GeoJSON:
-                return mapVisualization.data.map((feature) => (
-                  <GeoJSON
-                    geometry={feature.geometry}
-                    key={`${mapVisualization.id}_${feature.name}`}
-                    options={{
-                      // Todo: move geoJsonConfig to new dataselection config.ts when legacy map is removed
-                      style: geoJsonConfig[feature.name as any]?.style,
-                    }}
-                    onAdd={(geoJsonLayer) => {
-                      geoJsonLayer.bringToBack()
-                    }}
-                  />
-                ))
-              case DataSelectionMapVisualizationType.Markers:
-                return (
-                  <MarkerClusterGroup
-                    key={mapVisualization.id}
-                    markers={mapVisualization.data.map(({ latLng }) => latLng)}
-                  />
-                )
-              default:
-                return null
-            }
-          })
-        : null}
+      {mapVisualizations.map((mapVisualization) => {
+        switch (mapVisualization.type) {
+          case DataSelectionMapVisualizationType.GeoJSON:
+            return mapVisualization.data.map((feature) => (
+              <GeoJSON
+                geometry={feature.geometry}
+                key={`${mapVisualization.id}_${feature.name}`}
+                options={{
+                  // TODO: move geoJsonConfig to new dataselection config.ts when legacy map is removed
+                  style: geoJsonConfig[feature.name as any]?.style,
+                }}
+                onAdd={(geoJsonLayer) => {
+                  geoJsonLayer.bringToBack()
+                }}
+              />
+            ))
+          case DataSelectionMapVisualizationType.Markers:
+            return (
+              <MarkerClusterGroup
+                key={mapVisualization.id}
+                markers={mapVisualization.data.map(({ latLng }) => latLng)}
+              />
+            )
+          default:
+            return null
+        }
+      })}
     </>
   )
 }
