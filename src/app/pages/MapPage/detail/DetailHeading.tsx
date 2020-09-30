b59cbfcd919ce8103e6be863dbf0df52248a83a8
Jon Koops <jonkoops@gmail.com>
2020-09-30 11:17:06 +0200

    Add props to DetailHeading

--- src/app/pages/MapPage/detail/DetailHeading.tsx
+++ src/app/pages/MapPage/detail/DetailHeading.tsx
@@ -1,14 +1,13 @@
-import styled from 'styled-components'
 import { Heading, themeColor } from '@amsterdam/asc-ui'
-import React from 'react'
+import React, { ComponentProps } from 'react'
+import styled from 'styled-components'
 
 const StyledHeading = styled(Heading)`
   color: ${themeColor('secondary')};
   margin-bottom: 0.2em;
 `
 
-const DetailHeading: React.FC = ({ children, ...otherProps }) => (
-  // @ts-ignore
+const DetailHeading: React.FC<ComponentProps<typeof Heading>> = ({ children, ...otherProps }) => (
   <StyledHeading forwardedAs="h4" styleAs="h2" {...otherProps}>
     {children}
   </StyledHeading>
