f6a1f33c06c639d5a962234a50e365093ac65240
Jon Koops <jonkoops@gmail.com>
2020-10-06 14:01:01 +0200

    Clean up page template

--- src/app/components/PageTemplate/PageTemplate.tsx
+++ src/app/components/PageTemplate/PageTemplate.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
 import { Container } from '@amsterdam/asc-ui'
+import React, { FunctionComponent, ReactNode } from 'react'
+import styled from 'styled-components'
 import { PromiseResult, PromiseStatus } from '../../utils/usePromise'
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
@@ -10,31 +10,35 @@ const StyledLoadingSpinner = styled(LoadingSpinner)`
   top: 200px;
 `
 
-type Props = {
+export interface PageTemplateProps {
   result: PromiseResult<any>
   onRetry: () => void
   errorMessage?: string
 }
 
-const PageTemplate: React.FC<Props> = ({ result, onRetry, errorMessage, children }) => {
-  let component
-  if (result.status === PromiseStatus.Pending) {
-    component = <StyledLoadingSpinner />
-  }
+const PageTemplate: FunctionComponent<PageTemplateProps> = ({
+  result,
+  onRetry,
+  errorMessage,
+  children,
+}) => {
+  const component: ReactNode = (() => {
+    if (result.status === PromiseStatus.Pending) {
+      return <StyledLoadingSpinner />
+    }
 
-  if (result.status === PromiseStatus.Rejected) {
-    component = (
-      <ErrorMessage
-        message={errorMessage || 'Er is een fout opgetreden bij het laden van deze pagina'}
-        buttonLabel="Probeer opnieuw"
-        buttonOnClick={onRetry}
-      />
-    )
-  }
+    if (result.status === PromiseStatus.Rejected) {
+      return (
+        <ErrorMessage
+          message={errorMessage || 'Er is een fout opgetreden bij het laden van deze pagina'}
+          buttonLabel="Probeer opnieuw"
+          buttonOnClick={onRetry}
+        />
+      )
+    }
 
-  if (result.status === PromiseStatus.Fulfilled) {
-    component = children
-  }
+    return children
+  })()
 
   return <Container>{component}</Container>
 }
