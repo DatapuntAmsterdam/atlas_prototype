00397278bfadd528118b5d13ceac7418642ffb05
Jon Koops <jonkoops@gmail.com>
2020-10-14 15:50:55 +0200

    Clean up DataDetailPage

--- src/app/pages/DataDetailPage/DataDetailPage.tsx
+++ src/app/pages/DataDetailPage/DataDetailPage.tsx
@@ -1,27 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useParams } from 'react-router-dom'
 import { Container, Heading, themeColor, themeSpacing } from '@amsterdam/asc-ui'
+import React, { FunctionComponent } from 'react'
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 import { DetailInfo } from '../../../map/types/details'
 import PromiseResult from '../../components/PromiseTemplate/PromiseResult'
 import ShareBar from '../../components/ShareBar/ShareBar'
-import { getPanelTitle, HeadingWrapper, PanelContents } from '../MapPage/detail/DetailPanel'
 import DetailInfoBox from '../MapPage/detail/DetailInfoBox'
+import { getPanelTitle, HeadingWrapper, PanelContents } from '../MapPage/detail/DetailPanel'
 import useDataDetail from './useDataDetail'
-
-type Props = {
-  isLoading: boolean
-  user: object
-  endpoint: string
-  id: string
-  type: string
-  subType: string
-  previewPanorama?: object
-  isPreviewPanoramaLoading?: boolean
-  detailTemplateUrl?: string
-  detailData?: object
-  detailFilterSelection?: object
-}
 
 const DetailWrapper = styled(Container)`
   display: flex;
@@ -36,14 +22,15 @@ const DetailType = styled.strong`
   font-size: 21px;
 `
 
-interface Params extends DetailInfo {
+// TODO: 'subType' should be replaced with the 'subType' property on 'DetailInfo'
+// This should happen when the old Angular and Redux Router code has been deleted.
+interface DataDetailPageParams extends Omit<DetailInfo, 'subType'> {
   subtype: string
 }
 
-const Detail: React.FC<Props> = () => {
-  const { id: rawId, subtype: subType, type } = useParams<Params>()
+const DataDetailPage: FunctionComponent = () => {
+  const { id: rawId, subtype: subType, type } = useParams<DataDetailPageParams>()
   const id = rawId.includes('id') ? rawId.substr(2) : rawId
-
   const { result: promise, onRetry } = useDataDetail(id, subType, type)
 
   return (
@@ -63,4 +50,4 @@ const Detail: React.FC<Props> = () => {
   )
 }
 
-export default Detail
+export default DataDetailPage
