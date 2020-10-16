0121f916b93521e81cbf381348eb910cbf5cdcc7
Jon Koops <jonkoops@gmail.com>
2020-10-16 16:35:47 +0200

    Fix tests

--- src/map/services/map/parseDetailPath.test.ts
+++ src/map/services/map/parseDetailPath.test.ts
@@ -4,16 +4,14 @@ describe('parseDetailPath', () => {
   it('should parse the url', () => {
     expect(parseDetailPath('/foo/bar/1234')).toEqual({
       id: '1234',
-      subType: 'bar',
-      type: 'foo',
+      type: 'foo/bar',
     })
   })
 
   it('should should remove the id prefix', () => {
     expect(parseDetailPath('/foo/bar/id1234')).toEqual({
       id: '1234',
-      subType: 'bar',
-      type: 'foo',
+      type: 'foo/bar',
     })
   })
 })
