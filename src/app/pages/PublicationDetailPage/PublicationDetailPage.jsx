bf414230f901d55ea889f7d0407b213728d69c28
Jon Koops <jonkoops@gmail.com>
2020-11-11 16:30:09 +0100

    Add related articles to publication detail page
    This reverts commit 8103a1fbf2826b06fee639a1f2ca55cd1da2495a.

--- src/app/pages/PublicationDetailPage/PublicationDetailPage.jsx
+++ src/app/pages/PublicationDetailPage/PublicationDetailPage.jsx
@@ -6,12 +6,18 @@ import {
   Heading,
   Paragraph,
   Row,
+  List,
+  ListItem,
+  Link,
 } from '@amsterdam/asc-ui'
 import { useMatomo } from '@datapunt/matomo-tracker-react'
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link as RouterLink } from 'react-router-dom'
+import { generatePath } from 'react-router'
+import { routing } from '../../routes'
 import environment from '../../../environment'
 import { cmsConfig } from '../../../shared/config/config'
+import { routingKey } from '../../../shared/config/cms.config'
 import { toPublicationDetail } from '../../../store/redux-first-router/actions'
 import ContentContainer from '../../components/ContentContainer/ContentContainer'
 import DocumentCover from '../../components/DocumentCover/DocumentCover'
@@ -43,6 +49,7 @@ const PublicationDetailPage = () => {
     field_intro: intro,
     field_language: lang,
     slug,
+    related,
   } = results || {}
 
   const documentTitle = title && `Publicatie: ${title}`
@@ -97,6 +104,24 @@ const PublicationDetailPage = () => {
                   <EditorialContent>
                     {intro && <Paragraph strong dangerouslySetInnerHTML={{ __html: intro }} />}
                     {body && <CustomHTMLBlock body={body} />}
+                    {related?.length ? (
+                      <List>
+                        {related.map(({ id: linkId, title: linkTitle, type, to }) => (
+                          <ListItem key={linkId}>
+                            <Link
+                              as={RouterLink}
+                              to={generatePath(routing[routingKey[type]].path, {
+                                slug: to.payload.slug,
+                                id: to.payload.id,
+                              })}
+                              inList
+                            >
+                              {linkTitle}
+                            </Link>
+                          </ListItem>
+                        ))}
+                      </List>
+                    ) : null}
                   </EditorialContent>
                 </Column>
               </Column>
