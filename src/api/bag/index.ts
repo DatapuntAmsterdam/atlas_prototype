97fe7136f791ad52e4ec640351ad831906b77bc7
Chris van Mook <chrisvanmook@gmail.com>
2021-01-05 15:12:42 +0100

    Export modules directly

--- src/api/bag/index.ts
+++ src/api/bag/index.ts
@@ -1,8 +1,6 @@
-import * as ligplaats from './v1.1/ligplaats'
-import * as nummeraanduiding from './v1.1/nummeraanduiding'
-import * as pand from './v1.1/pand'
-import * as standplaats from './v1.1/standplaats'
-import * as verblijfsobject from './v1.1/verblijfsobject'
-import * as woonplaats from './v1.1/woonplaats'
-
-export { ligplaats, nummeraanduiding, pand, standplaats, verblijfsobject, woonplaats }
+export * as ligplaats from './v1.1/ligplaats'
+export * as nummeraanduiding from './v1.1/nummeraanduiding'
+export * as pand from './v1.1/pand'
+export * as standplaats from './v1.1/standplaats'
+export * as verblijfsobject from './v1.1/verblijfsobject'
+export * as woonplaats from './v1.1/woonplaats'
