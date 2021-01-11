a1d73aa7440c33f7324c47c97b00a4978d42c48c
JJM van Deursen <janjaap@users.noreply.github.com>
2021-01-11 15:48:25 +0100

    Adds global handlers

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -17,3 +17,32 @@ global.L = L
 
 // Mock the window.fetch function
 global.fetch = require('jest-fetch-mock')
+
+/* tokens generated with https://www.jsonwebtoken.io/ */
+// token contains 'exp' prop with a date in the past
+const expiredToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjZhNTc3NzZlLTczYWYtNDM3ZS1hMmJiLThmYTkxYWVhN2QxYSIsImlhdCI6MTU4ODE2Mjk2MywiZXhwIjoxMjQyMzQzfQ.RbJHkXRPmFZMYDJs-gxhk7vWYlIYZi8uik83Q0V1nas'
+
+// token doesn't have 'exp' prop
+const invalidToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+// token contains 'exp' prop with a date far into the future
+const validToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6ImMxOWRhNDgwLTAyM2UtNGM2YS04NDM2LWNhMzNkYzZjYzVlMyIsImlhdCI6MTU4ODE2NDUyMCwiZXhwIjoxNTg4MTY4MTQ1MH0.LMA3E950H0EACrvME7Gps1Y-Q43Fux1q8YCJUl9pbYE'
+
+global.unsetAuthentication = () => {
+  global.sessionStorage.removeItem('accessToken')
+}
+
+global.setExpiredAuthentication = () => {
+  global.sessionStorage.setItem('accessToken', expiredToken)
+}
+
+global.setValidAuthentication = () => {
+  global.sessionStorage.setItem('accessToken', validToken)
+}
+
+global.setInvalidAuthentication = () => {
+  global.sessionStorage.setItem('accessToken', invalidToken)
+}
