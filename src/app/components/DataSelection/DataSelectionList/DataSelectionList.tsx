afd274b21173dcde9cf2aeee34cf7cd8dadff80e
Jon Koops <jonkoops@gmail.com>
2021-01-26 13:12:57 +0100

    Use standard method for links

--- src/app/components/DataSelection/DataSelectionList/DataSelectionList.tsx
+++ src/app/components/DataSelection/DataSelectionList/DataSelectionList.tsx
@@ -1,22 +1,14 @@
-import { FunctionComponent } from 'react'
-import { generatePath, Link as RouterLink } from 'react-router-dom'
-import styled from 'styled-components'
 import { Link, themeSpacing } from '@amsterdam/asc-ui'
-import { LocationDescriptor } from 'history'
+import { FunctionComponent } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import styled from 'styled-components'
 import { getDetailPageData } from '../../../../store/redux-first-router/actions'
+import buildDetailUrl from '../../../pages/MapPage/detail/buildDetailUrl'
 import DataSelectionFormatter from '../DataSelectionFormatter/DataSelectionFormatter'
-import { routing } from '../../../routes'
 
 const StyledListItem = styled.li`
   margin-bottom: ${themeSpacing(2)};
 `
-
-export const toDetailPage = (endpoint: string): LocationDescriptor => {
-  const { type, subtype, id } = getDetailPageData(endpoint)
-  return {
-    pathname: generatePath(routing.dataDetail.path, { type, subtype, id }),
-  }
-}
 
 // TODO: Accurately determine the types in this interface.
 export interface DataSelectionListProps {
@@ -37,7 +29,7 @@ const DataSelectionList: FunctionComponent<DataSelectionListProps> = ({ content 
     <ul>
       {content.body.map((row) => (
         <StyledListItem key={row.content[0]}>
-          <Link as={RouterLink} to={toDetailPage(row.detailEndpoint)} inList>
+          <Link as={RouterLink} to={buildDetailUrl(getDetailPageData(row.detailEndpoint))} inList>
             {/*
             // @ts-ignore */}
             <DataSelectionFormatter
