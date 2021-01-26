ca6cf51bd1461b7e5c00e92713b09e2e97940bb3
Jon Koops <jonkoops@gmail.com>
2021-01-26 10:33:52 +0100

    Combine condition

--- src/app/utils/pickLinkComponent.ts
+++ src/app/utils/pickLinkComponent.ts
@@ -3,11 +3,7 @@ import ReduxRouterLink, { To } from 'redux-first-router-link'
 import { Link as RouterLink } from 'react-router-dom'
 
 export default function pickLinkComponent(to: To | LocationDescriptorObject) {
-  if (typeof to === 'object' && 'pathname' in to) {
-    return RouterLink
-  }
-
-  if (typeof to === 'string') {
+  if (typeof to === 'string' || (typeof to === 'object' && 'pathname' in to)) {
     return RouterLink
   }
 
