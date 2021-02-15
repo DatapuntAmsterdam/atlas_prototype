621689866c17b41686ab4acf0d37ca8de6838646
JJM van Deursen <janjaap@users.noreply.github.com>
2021-02-15 08:36:38 +0100

    (Chore) Increase coverage

---
+++ src/shared/services/geometry/geometry.test.ts
@@ -0,0 +1,10 @@
+import getGeometry, { isVestigingAmsterdam } from './geometry'
+
+describe('geometry', () => {
+  describe('isVestigingAmsterdam', () => {
+    it('returns true', () => {})
+    it('returns false', () => {})
+  })
+
+  // it('', () => {})
+})
---
+++ src/shared/services/is-object.ts
@@ -0,0 +1,3 @@
+const isObject = (value: any) => value?.constructor?.name === 'Object'
+
+export default isObject
