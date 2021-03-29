0f4415502cc843acbfaed092d51e1580b756998c
Aram Limpens <a.limpens@amsterdam.nl>
2021-03-29 12:23:59 +0200

    Added a test case for kaarten route

--- src/app/pages/MapPage/detail/buildDetailUrl.test.tsx
+++ src/app/pages/MapPage/detail/buildDetailUrl.test.tsx
@@ -21,6 +21,25 @@ describe('return detail panel path', () => {
     locationSpy.mockRestore()
   })
 
+  it('should return a path starting with "/data/" when pathname includes "kaarten"', () => {
+    const locationSpy = jest.spyOn(window, 'location', 'get').mockReturnValue({
+      ...window.location,
+      ...{ pathname: '/kaarten' },
+    })
+
+    const params = {
+      type: 'foo',
+      subtype: 'bar',
+      id: 'baz',
+    }
+
+    const location = buildDetailUrl(params)
+
+    expect(location.pathname).toBe('/data/foo/bar/baz/')
+
+    locationSpy.mockRestore()
+  })
+
   it('should return a path starting with "/data/" when pathname does not include "kaart"', () => {
     const params = {
       type: 'foo',
