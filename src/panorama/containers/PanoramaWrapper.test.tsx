43b5ee151c46be9d8a7de6b7c215935b722c0b67
Chris van Mook <chrisvanmook@gmail.com>
2021-04-12 16:56:19 +0200

    Fixed unit test

--- src/panorama/containers/PanoramaWrapper.test.tsx
+++ src/panorama/containers/PanoramaWrapper.test.tsx
@@ -1,14 +1,21 @@
 import { render } from '@testing-library/react'
+import * as reactRedux from 'react-redux'
 import PanoramaWrapper from './PanoramaWrapper'
-import * as panoSelectors from '../ducks/selectors'
 import { ForbiddenError } from '../../shared/services/api/customError'
+import withAppContext from '../../app/utils/withAppContext'
+
+jest.mock('react-redux', () => ({
+  // @ts-ignore
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+}))
 
 describe('PanoramaWrapper', () => {
   it('should not render the PanoramaContainer when error is set', () => {
     const errorSelectorMock = jest
-      .spyOn(panoSelectors, 'getPanoramaError')
-      .mockReturnValue(new ForbiddenError(403, 'forbidden'))
-    const { queryByTestId } = render(<PanoramaWrapper />)
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation(() => new ForbiddenError(403, 'forbidden'))
+    const { queryByTestId } = render(withAppContext(<PanoramaWrapper />))
     expect(errorSelectorMock).toHaveBeenCalledTimes(1)
     expect(queryByTestId('panoramaContainer')).toBeNull()
   })
