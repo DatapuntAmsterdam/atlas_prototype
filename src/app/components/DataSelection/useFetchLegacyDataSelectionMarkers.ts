398a2f375906548dab40e8d109a7a947c5a70bb1
Jon Koops <jonkoops@gmail.com>
2021-04-13 10:56:48 +0200

    Remove redundant useCallback calls from data and marker fetching hooks

--- src/app/components/DataSelection/useFetchLegacyDataSelectionMarkers.ts
+++ src/app/components/DataSelection/useFetchLegacyDataSelectionMarkers.ts
@@ -1,16 +1,15 @@
-import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { polygonParam, ViewMode, viewParam } from '../../pages/MapPage/query-params'
-import useParam from '../../utils/useParam'
-import { useDataSelection } from './DataSelectionContext'
-import useLegacyDataselectionConfig from './useLegacyDataselectionConfig'
+import { getMapBoundingBox, getMapZoom } from '../../../map/ducks/map/selectors'
+import BOUNDING_BOX from '../../../map/services/bounding-box.constant'
 import {
   fetchMarkersRequest,
   fetchMarkersSuccess,
 } from '../../../shared/ducks/data-selection/actions'
 import { createFiltersObject } from '../../../shared/services/data-selection/normalizations'
-import BOUNDING_BOX from '../../../map/services/bounding-box.constant'
-import { getMapBoundingBox, getMapZoom } from '../../../map/ducks/map/selectors'
+import { polygonParam, ViewMode, viewParam } from '../../pages/MapPage/query-params'
+import useParam from '../../utils/useParam'
+import { useDataSelection } from './DataSelectionContext'
+import useLegacyDataselectionConfig from './useLegacyDataselectionConfig'
 
 /**
  * Note: a lot of this logic has been refactored from several sagas and put into this one
@@ -26,31 +25,31 @@ const useFetchLegacyDataSelectionMarkers = () => {
   const { activeFilters, totalResults } = useDataSelection()
   const { currentDatasetConfig } = useLegacyDataselectionConfig()
 
-  const fetchMarkers = useCallback(
-    async (signal: AbortSignal) => {
-      if (!currentDatasetConfig) {
-        return null
-      }
-      const markersShouldBeFetched =
-        view === ViewMode.Split &&
-        totalResults !== 0 &&
-        totalResults <= currentDatasetConfig.MAX_NUMBER_OF_CLUSTERED_MARKERS &&
-        (polygon?.polygon.length !== 0 || activeFilters)
+  const fetchMarkers = async (signal: AbortSignal) => {
+    if (!currentDatasetConfig) {
+      return null
+    }
 
-      if (markersShouldBeFetched) {
-        dispatch(fetchMarkersRequest())
-        const mapMarkers = await currentDatasetConfig.CUSTOM_API.getMarkers(
-          signal,
-          currentDatasetConfig,
-          createFiltersObject(activeFilters),
-          mapZoom,
-          mapBoundingBox ?? BOUNDING_BOX.COORDINATES,
-        )
-        dispatch(fetchMarkersSuccess(mapMarkers))
-      }
-    },
-    [view, polygon, mapZoom, mapBoundingBox, activeFilters, totalResults, currentDatasetConfig],
-  )
+    const markersShouldBeFetched =
+      view === ViewMode.Split &&
+      totalResults !== 0 &&
+      totalResults <= currentDatasetConfig.MAX_NUMBER_OF_CLUSTERED_MARKERS &&
+      (polygon?.polygon.length !== 0 || activeFilters)
+
+    if (markersShouldBeFetched) {
+      return null
+    }
+
+    dispatch(fetchMarkersRequest())
+    const mapMarkers = await currentDatasetConfig.CUSTOM_API.getMarkers(
+      signal,
+      currentDatasetConfig,
+      createFiltersObject(activeFilters),
+      mapZoom,
+      mapBoundingBox ?? BOUNDING_BOX.COORDINATES,
+    )
+    dispatch(fetchMarkersSuccess(mapMarkers))
+  }
 
   return { fetchMarkers }
 }
