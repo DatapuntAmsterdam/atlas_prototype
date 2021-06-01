bfdc66a3119c9395ba0520920604e0bd3dfa86c5
Jon Koops <jonkoops@gmail.com>
2021-06-01 13:42:59 +0200

    Update SearchSort with new param logic

--- src/app/pages/SearchPage/SearchSort.tsx
+++ src/app/pages/SearchPage/SearchSort.tsx
@@ -1,10 +1,10 @@
 import { breakpoint, Label, Select, themeColor, themeSpacing } from '@amsterdam/asc-ui'
 import { useMatomo } from '@datapunt/matomo-tracker-react'
+import type { ChangeEvent, FunctionComponent } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
-import type { FunctionComponent } from 'react'
-import useBuildQueryString from '../../utils/useBuildQueryString'
-import { pageParam, Sort, sortParam } from './query-params'
+import toSearchParams from '../../utils/toSearchParams'
+import { pageParam, Sort, SortOrder, sortParam } from './query-params'
 
 const SelectboxWrapper = styled.div`
   display: flex;
@@ -46,7 +46,6 @@ const SearchSort: FunctionComponent<SearchSortProps> = ({ sort, isOverviewPage, 
   const { trackEvent } = useMatomo()
   const history = useHistory()
   const location = useLocation()
-  const { buildQueryString } = useBuildQueryString()
 
   return (
     <SelectboxWrapper>
@@ -54,33 +53,46 @@ const SearchSort: FunctionComponent<SearchSortProps> = ({ sort, isOverviewPage, 
         <StyledSelect
           id="sort-select"
           data-testid="sort-select"
-          value={sort ? `${sort.field}:${sort.order}` : undefined}
+          value={sort ? sortParam.encode(sort) : ''}
           disabled={disabled}
-          onChange={(e) => {
-            // @ts-ignore
-            const [field, order] = e.target.value.split(':')
+          onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+            const sortRaw = event.target.value
+            const newSort = sortRaw ? sortParam.decode(sortRaw) : null
+
             trackEvent({
               category: 'search',
               action: 'sort',
-              // @ts-ignore
-              name: e.target.value,
+              name: sortRaw,
             })
+
             history.replace({
               pathname: location.pathname,
-              search: buildQueryString([
-                // @ts-ignore
+              search: toSearchParams([
+                [sortParam, newSort],
                 [pageParam, 1],
-                // @ts-ignore
-                [sortParam, { field, order }],
-              ]),
+              ]).toString(),
             })
           }}
         >
           {!isOverviewPage && <option value="">Relevantie</option>}
-          <option value={!isOverviewPage ? 'date:desc' : ''}>Publicatiedatum aflopend</option>
-          <option value="date:asc">Publicatiedatum oplopend</option>
-          <option value="title:asc">Titel A-Z</option>
-          <option value="title:desc">Titel Z-A</option>
+          <option
+            value={
+              !isOverviewPage
+                ? sortParam.encode({ field: 'date', order: SortOrder.Descending })
+                : ''
+            }
+          >
+            Publicatiedatum aflopend
+          </option>
+          <option value={sortParam.encode({ field: 'date', order: SortOrder.Ascending })}>
+            Publicatiedatum oplopend
+          </option>
+          <option value={sortParam.encode({ field: 'title', order: SortOrder.Ascending })}>
+            Titel A-Z
+          </option>
+          <option value={sortParam.encode({ field: 'title', order: SortOrder.Descending })}>
+            Titel Z-A
+          </option>
         </StyledSelect>
       </StyledLabel>
     </SelectboxWrapper>
