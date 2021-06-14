96fdc5c9d12a07ff13f951a2313672a919c9b7e2
Thomas Mills <mills.thoma@gmail.com>
2021-06-14 15:48:19 +0200

    Drop file list variable in Dossier Restricted files modal and move map function into render

--- src/app/pages/ConstructionDossierPage/components/RestrictedFilesModal/RestrictedFilesModal.tsx
+++ src/app/pages/ConstructionDossierPage/components/RestrictedFilesModal/RestrictedFilesModal.tsx
@@ -28,39 +28,39 @@ const RestrictedFilesModal: FunctionComponent<RestrictedFilesModalProps> = ({
   files,
   onClose,
   onDownload,
-}) => {
-  const fileList = files.map(({ filename, url }) => <ListItem key={url}>{filename}</ListItem>)
-
-  return (
-    <Modal open onClose={onClose}>
-      <TopBar>
-        <Heading as="h4">
-          De volgende documenten zijn uitgesloten van uw download
-          <Button
-            variant="blank"
-            title="Sluit"
-            type="button"
-            size={30}
-            onClick={onClose}
-            icon={<Close />}
-          />
-        </Heading>
-      </TopBar>
-      <Divider />
-      <ModalBody>
-        <>
-          <Paragraph>
-            U heeft niet de bevoegdheden om de volgende beveiligde documenten te downloaden:
-          </Paragraph>
-          <List variant="bullet">{fileList}</List>
-          <Paragraph>De overige bestanden kunt u wel in één keer downloaden.</Paragraph>
-          <Button type="button" variant="primary" onClick={onDownload}>
-            Download het dossier
-          </Button>
-        </>
-      </ModalBody>
-    </Modal>
-  )
-}
+}) => (
+  <Modal open onClose={onClose}>
+    <TopBar>
+      <Heading as="h4">
+        De volgende documenten zijn uitgesloten van uw download
+        <Button
+          variant="blank"
+          title="Sluit"
+          type="button"
+          size={30}
+          onClick={onClose}
+          icon={<Close />}
+        />
+      </Heading>
+    </TopBar>
+    <Divider />
+    <ModalBody>
+      <>
+        <Paragraph>
+          U heeft niet de bevoegdheden om de volgende beveiligde documenten te downloaden:
+        </Paragraph>
+        <List variant="bullet">
+          {files.map(({ filename, url }) => (
+            <ListItem key={url}>{filename}</ListItem>
+          ))}
+        </List>
+        <Paragraph>De overige bestanden kunt u wel in één keer downloaden.</Paragraph>
+        <Button type="button" variant="primary" onClick={onDownload}>
+          Download het dossier
+        </Button>
+      </>
+    </ModalBody>
+  </Modal>
+)
 
 export default RestrictedFilesModal
